7f766a9b318eb6218115b7bf3de74097e0624b7a
Md. Zihad <md.zihad0246@gmail.com>
2024-02-08 02:23:38 +0600

    added num fundamentals

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ console.log("Hello World");
 
 // Variables 
 
-var a=12;
-var b='twelve';
-var c = 5.6;
-var d = true;
-var e;
-var f=null;
+var a=12; // numeric datatype
+var b='twelve'; // string datatype
+var b1="twelve"; // string datatype
+var b2=`twelve`; // string datatype
+var c = 5.6; // float datatype
+var d = true; // boolean 
+var e; // undefined
+var f=null; // null
 
-console.log(f);
+// console.log(typeof(a));
+
+console.log(a+e);
---
+++ numbersFundamentals.js
@@ -0,0 +1,9 @@
+var num1 = 34;
+var num2 = '20';
+var num3 = '20.65';
+
+
+console.log(typeof(num1))
+console.log(typeof(num2))
+console.log(typeof(num3))
+console.log(num1+parseFloat(num3))
